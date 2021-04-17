078425db7120047771a88ae8b832e5c9bf31512a
Mathieu Beligon <mathieu@feedly.com>
2021-04-17 19:43:35 -0400

    [research] (split_mnist_demo) use seeds

--- tricicl/constants.py
+++ tricicl/constants.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
+from tricicl.utils.path import safe_path
+
 PROJECT_DIR = Path(__file__).parent.parent
 LOGS_DIR = PROJECT_DIR / "logs"
-TB_DIR = LOGS_DIR / "tb"
+TB_DIR = safe_path(LOGS_DIR / "tb")
+CSV_DIR = safe_path(LOGS_DIR / "csv")
 
-TB_DIR.mkdir(exist_ok=True, parents=True)
+
+SEEDS = [42, 51, 441390, 6584, 601065, 845929, 960932, 478844, 565362, 388222]
--- tricicl/research/mathieu/benchmark_split_mnist.py
+++ tricicl/research/mathieu/benchmark_split_mnist.py
@@ -1,26 +1,30 @@
+from typing import List
+
 import torch
 from avalanche.benchmarks.classic import SplitMNIST
-from avalanche.evaluation.metrics import (StreamConfusionMatrix,
-                                          accuracy_metrics)
-from avalanche.logging import InteractiveLogger, TensorboardLogger
+from avalanche.evaluation.metrics import StreamConfusionMatrix, accuracy_metrics
+from avalanche.logging import TensorboardLogger
 from avalanche.training import EvaluationPlugin
 from avalanche.training.plugins import LwFPlugin, ReplayPlugin, StrategyPlugin
 from avalanche.training.strategies import Naive
 from torch.nn import CrossEntropyLoss
 from torch.optim import SGD
+from tqdm import tqdm
 
-from tricicl.constants import TB_DIR
+from tricicl.constants import SEEDS, TB_DIR
 from tricicl.metrics.confusion_matrix import SortedCMImageCreator
 from tricicl.models.simple_mlp import SimpleMLP
 from tricicl.strategies.icarl import make_icarl_plugins
+from tricicl.utils.time import create_time_id
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-perm_mnist = SplitMNIST(n_experiences=5)
 
-
-def evaluate(name: str, plugins: list[StrategyPlugin]):
+def evaluate_on_seed(name: str, plugins: List[StrategyPlugin], seed: int):
+    perm_mnist = SplitMNIST(n_experiences=5, seed=seed)
     model = SimpleMLP(n_classes=perm_mnist.n_classes, input_size=28 * 28)
+
+    tb_logger = TensorboardLogger(TB_DIR + f"/split_mnist/{name}/{seed}_{create_time_id()}")
 
     cl_strategy = Naive(
         model,
@@ -34,9 +38,9 @@ def evaluate(name: str, plugins: list[StrategyPlugin]):
         evaluator=EvaluationPlugin(
             accuracy_metrics(experience=True, stream=True),
             StreamConfusionMatrix(
-                num_classes=perm_mnist.n_classes, image_creator=SortedCMImageCreator(perm_mnist.classes_order),
+                num_classes=perm_mnist.n_classes, image_creator=SortedCMImageCreator(perm_mnist.classes_order)
             ),
-            loggers=[InteractiveLogger(), TensorboardLogger(TB_DIR / "split_mnist" / name)],
+            loggers=[tb_logger],
         ),
     )
 
@@ -44,10 +48,13 @@ def evaluate(name: str, plugins: list[StrategyPlugin]):
         cl_strategy.train(train_task, num_workers=0)
         cl_strategy.eval(perm_mnist.test_stream[:i])
 
+    tb_logger.writer.flush()
+
 
 if __name__ == "__main__":
-    evaluate("iCaRL", make_icarl_plugins(memory_size=200))
-    evaluate("LwF", [LwFPlugin()])
-    evaluate("naive", [])
-    evaluate("replay", [ReplayPlugin()])
-    evaluate("hybrid1", [ReplayPlugin(), LwFPlugin()])
+    for seed in tqdm(SEEDS, unit="seed", desc="Training and evaluating methods on seeds"):
+        evaluate_on_seed("iCaRL", make_icarl_plugins(memory_size=200), seed)
+        evaluate_on_seed("LwF", [LwFPlugin()], seed)
+        evaluate_on_seed("naive", [], seed)
+        evaluate_on_seed("replay", [ReplayPlugin()], seed)
+        evaluate_on_seed("hybrid1", [ReplayPlugin(), LwFPlugin()], seed)
