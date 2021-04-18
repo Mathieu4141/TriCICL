2cf0252d67093d951e2ba7be2758d398cccc7e3b
Mathieu Beligon <mathieu@feedly.com>
2021-04-18 18:55:18 -0400

    [GCStorage] fix: need to specify project name

--- src/tricicl/storage/gcloud.py
+++ src/tricicl/storage/gcloud.py
@@ -1,14 +1,14 @@
 from pathlib import Path, PurePath
 
-from google.cloud.storage import Bucket, Client
-from memoized_property import memoized_property
+from google.cloud.storage import Client
 
 from tricicl.storage.remote_storage import RemoteStorageABC
 
 
 class GCStorage(RemoteStorageABC):
-    def __init__(self, bucket_name: str = "tricicl-public"):
-        self.bucket_name = bucket_name
+    def __init__(self, project: str = "mathieu-tricicl", bucket_name: str = "tricicl-public"):
+        self.client = Client(project=project)
+        self.bucket = self.client.bucket(bucket_name)
 
     def upload_file(self, local_path: Path, remote_path: PurePath):
         blob = self.bucket.blob(str(remote_path), chunk_size=10 * 1024 * 1024)
@@ -20,11 +20,3 @@ class GCStorage(RemoteStorageABC):
         if blob is None:
             raise FileNotFoundError(f"{remote_path} is not on gcloud bucket")
         blob.download_to_filename(str(local_path), timeout=60 * 5)
-
-    @memoized_property
-    def client(self) -> Client:
-        return Client()
-
-    @memoized_property
-    def bucket(self) -> Bucket:
-        return self.client.bucket(self.bucket_name)
