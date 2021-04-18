995741941948ff9ecc19d0039a4b81b63bb3e2c8
Mathieu Beligon <mathieu@feedly.com>
2021-04-18 18:27:37 -0400

    [gcloud] Make different deployment files for GPU and CPU

--- src/research/mathieu/gcloud/launch_split_mnist_demo.sh
+++ src/research/mathieu/gcloud/launch_split_mnist_demo.sh
@@ -3,22 +3,20 @@ METHOD=naive
 
 cd ../../../..
 
-pwd
-
 DATETIME=$(date +%Y%m%d_%H%M%S)
 
 JOB_DIR=gs://tricicl-public/packages/${DATETIME}
 
 poetry build -f wheel
 
-for METHOD in naive; do
-  for SEED in 1; do
+for METHOD in naive hybrid1; do
+  for SEED in 1 42; do
     JOB_NAME=${TASK_NAME}_${METHOD}_${SEED}_${DATETIME}
 
   #  gcloud ai-platform local train \
     echo "Starting job ${JOB_NAME}"
-    gcloud ai-platform jobs submit training ${JOB_NAME} \
-      --config src/research/mathieu/gcloud/split_mnist_demo.yaml \
+    gcloud ai-platform jobs submit training "${JOB_NAME}" \
+      --config src/research/mathieu/gcloud/split_mnist_demo_cpu.yaml \
       --labels task="${TASK_NAME}",method="${METHOD}" \
       --job-dir "${JOB_DIR}" \
       --packages dist/tricicl-0.1.0-py3-none-any.whl \
@@ -29,3 +27,4 @@ for METHOD in naive; do
   done
 done
 
+poetry run tensorboard --logdir gs://tricicl-public/logs/tb/${TASK_NAME}
