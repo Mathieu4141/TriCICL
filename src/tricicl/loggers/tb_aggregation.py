7dcbc54e81c2b84c785f23774a1fad310c659ea2
Mathieu Beligon <mathieu@feedly.com>
2021-04-18 23:09:26 -0400

    [logger] (TBAggregation) use proper steps

--- src/tricicl/loggers/tb_aggregation.py
+++ src/tricicl/loggers/tb_aggregation.py
@@ -1,11 +1,9 @@
-import os
-from glob import glob
+from collections import defaultdict
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, Iterable, List
+from typing import Dict, Iterable, List, Tuple
 
-import numpy
-from more_itertools import flatten
-from numpy import array, ndarray
+from numpy import array, mean, ndarray
 from pandas import DataFrame
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 from torch.utils.tensorboard import SummaryWriter
@@ -13,154 +11,96 @@ from torch.utils.tensorboard import SummaryWriter
 from tricicl.constants import CSV_DIR, TB_DIR
 from tricicl.storage.local_storage import LocalStorage
 from tricicl.storage.remote_storage import RemoteStorageABC, SyncPath
+from tricicl.utils.path import empty_dir
 
 
 def aggregate_task_results(task_name: str, remote_storage: RemoteStorageABC = LocalStorage()):
     remote_storage.download_directory(SyncPath.from_local(TB_DIR / task_name))
-    for algo_dir in _get_all_algo_dirs(task_name):
-        _aggregate_algo_tb_results(algo_dir)
-    _aggregate_task_results_to_csv(task_name)
+    results = _load_task_results(task_name)
+    _aggregate_task_results_to_csv(task_name, results)
+    _aggregate_task_results_to_tb(task_name, results)
 
 
 def get_aggregate_csv_file(task_name: str) -> Path:
     return (CSV_DIR / task_name).with_suffix(".csv")
 
 
-def _aggregate_algo_tb_results(algo_dir: Path):
-    events_dict = read_tb_events(str(algo_dir / "*"))
-    reduced_events = reduce_events(events_dict, ["mean"])
+@dataclass
+class RunResult:
+    seed: str
+    steps: ndarray
+    values: ndarray
 
-    write_tb_events(reduced_events, str(algo_dir), overwrite=True)
+    def __iter__(self) -> Iterable[Tuple[int, float]]:
+        return zip(self.steps, self.values)
 
 
-def _aggregate_task_results_to_csv(task_name: str):
+@dataclass
+class AlgoResults:
+    name: str
+    metric_name2results: Dict[str, List[RunResult]] = field(default_factory=lambda: defaultdict(list))
+
+    def __iter__(self) -> Iterable[Tuple[str, List[RunResult]]]:
+        return self.metric_name2results.items().__iter__()
+
+
+def _load_task_results(task_name: str) -> List[AlgoResults]:
+    return [_load_algo_results(algo_dir) for algo_dir in _get_all_algo_dirs(task_name)]
+
+
+def _load_algo_results(algo_dir: Path) -> AlgoResults:
+    res = AlgoResults(algo_dir.name)
+    for run_dir in algo_dir.glob("*"):
+        summary = EventAccumulator(str(run_dir)).Reload()
+        for metric_name in summary.Tags()["scalars"]:
+            summary.Scalars(metric_name)
+            res.metric_name2results[metric_name].append(
+                RunResult(
+                    seed=run_dir.name,
+                    steps=array([event.step for event in summary.Scalars(metric_name)]),
+                    values=array([event.value for event in summary.Scalars(metric_name)]),
+                )
+            )
+    return res
+
+
+def _aggregate_task_results_to_csv(task_name: str, task_results: List[AlgoResults]):
     df = DataFrame.from_records(
-        flatten(
-            _get_run_records(run_dir) for algo_dir in _get_all_algo_dirs(task_name) for run_dir in algo_dir.glob("*")
-        )
+        [
+            {
+                "run_seed": result.seed,
+                "run_algo": algo_results.name,
+                "metric": metric_name,
+                "step": step,
+                "value": value,
+            }
+            for algo_results in task_results
+            for metric_name, results in algo_results
+            for result in results
+            for step, value in result
+        ]
     )
     df.to_csv(get_aggregate_csv_file(task_name), index=False)
 
 
-def _get_run_records(run_dir: Path) -> List[Dict[str, Any]]:
-    summary = EventAccumulator(str(run_dir)).Reload()
-    return [
-        {
-            "run_seed": run_dir.name,
-            "run_algo": run_dir.parent.name,
-            "metric": tag,
-            "step": event.step,
-            "value": event.value,
-        }
-        for tag in summary.Tags()["scalars"]
-        for event in summary.Scalars(tag)
-    ]
+def _aggregate_task_results_to_tb(task_name: str, task_results: List[AlgoResults]):
+    for algo_results in task_results:
+        _aggregate_algo_results_to_tb(task_name, algo_results)
+
+
+def _aggregate_algo_results_to_tb(task_name: str, algo_results: AlgoResults):
+    with SummaryWriter(str(empty_dir(TB_DIR / task_name / (algo_results.name + "-mean")))) as writer:
+        for metric_name, results in algo_results:
+            for step, value in _average_results(results):
+                writer.add_scalar(metric_name, value, step)
+
+
+def _average_results(results: List[RunResult]) -> RunResult:
+    steps = results[0].steps
+    for res in results[:1]:
+        assert (steps == res.steps).all()
+    return RunResult(seed="mean", steps=steps, values=mean([r.values for r in results], axis=0))
 
 
 def _get_all_algo_dirs(task_name: str) -> Iterable[Path]:
-    return (
-        algo_dir
-        for algo_dir in (TB_DIR / task_name).glob("*")
-        if not algo_dir.name.endswith("-mean") and not algo_dir.name == "aggregate"
-    )
-
-
-# Functions below are taken from https://pypi.org/project/tensorboard-reducer/
-# There is a syntax error in python37 for tensorboard reducer
-def read_tb_events(indirs_glob: str) -> Dict[str, ndarray]:
-    indirs = glob(indirs_glob)
-    assert len(indirs) > 0, f"No runs found for glob pattern '{indirs_glob}'"
-
-    summary_iterators = [EventAccumulator(dirname).Reload() for dirname in indirs]
-
-    tags = summary_iterators[0].Tags()["scalars"]
-
-    for iterator in summary_iterators:
-        # assert all runs have the same tags for scalar data
-        assert iterator.Tags()["scalars"] == tags
-
-    out_dict = {t: [] for t in tags}
-
-    for tag in tags:
-        for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
-            assert len({e.step for e in events}) == 1
-
-            out_dict[tag].append([e.value for e in events])
-
-    return {key: array(val) for key, val in out_dict.items()}
-
-
-def reduce_events(events_dict: Dict[str, ndarray], reduce_ops: List[str]) -> Dict[str, Dict[str, ndarray]]:
-    reductions = {}
-
-    for op in reduce_ops:
-
-        reductions[op] = {}
-
-        for tag, arr in events_dict.items():
-
-            reduce_op = getattr(numpy, op)
-
-            reductions[op][tag] = reduce_op(arr, axis=-1)
-
-    return reductions
-
-
-def write_tb_events(
-    data_to_write: Dict[str, Dict[str, ndarray]],
-    outdir: str,
-    overwrite: bool = False,
-):
-    # handle std reduction separately as we use writer.add_scalars to write mean +/- std
-    if "std" in data_to_write.items():
-
-        assert (
-            "mean" in data_to_write.items()
-        ), "cannot write data for std reduction as mean+/-std without mean reduction"
-
-        std_dict = data_to_write.pop("std")
-        mean_dict = data_to_write["mean"]
-
-        std_dir = f"{outdir}-std"
-
-        force_rm_or_raise(std_dir, overwrite)
-
-        writer = SummaryWriter(std_dir)
-
-        for (tag, means), stds in zip(mean_dict.items(), std_dict.values()):
-            for idx, (mean, std) in enumerate(zip(means, stds)):
-                writer.add_scalars(tag, {"mean+std": mean + std, "mean-std": mean - std}, idx)
-
-        writer.close()
-
-    # loop over each reduce operation (e.g. mean, min, max, median)
-    for op, events_dict in data_to_write.items():
-
-        op_outdir = f"{outdir}-{op}"
-
-        force_rm_or_raise(op_outdir, overwrite)
-
-        writer = SummaryWriter(op_outdir)
-
-        for tag, data in events_dict.items():
-            for idx, scalar in enumerate(data):
-                writer.add_scalar(tag, scalar, idx)
-
-        # Important for allowing write_events() to overwrite. Without it,
-        # try_rmtree will raise OSError: [Errno 16] Device or resource busy
-        # trying to delete the existing outdir.
-        writer.close()
-
-
-def force_rm_or_raise(path: str, overwrite: bool) -> None:
-    if os.path.exists(path):  # True if dir is either file or directory
-
-        # for safety, check dir is either TensorBoard run or CSV file
-        # to make it harder to delete files not created by this program
-        is_csv_file = path.endswith(".csv")
-        is_tb_run_dir = os.path.isdir(path) and os.listdir(path)[0].startswith("events.out")
-
-        if overwrite and (is_csv_file or is_tb_run_dir):
-            os.system(f"rm -rf {path}")
-        else:
-            raise FileExistsError(f"'{path}' already exists, pass overwrite=True (-o in CLI) to proceed anyway")
+    return (algo_dir for algo_dir in (TB_DIR / task_name).glob("*") if not algo_dir.name.endswith("-mean"))
--- src/tricicl/utils/path.py
+++ src/tricicl/utils/path.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import rmtree
 from typing import Union
 
 
@@ -6,3 +7,8 @@ def safe_path(p: Union[str, Path]):
     p = Path(p)
     p.mkdir(exist_ok=True, parents=True)
     return p
+
+
+def empty_dir(p: Path) -> Path:
+    rmtree(p, ignore_errors=True)
+    return safe_path(p)
