b71780cdec2bf2857ce0abbf7e829f6c85129f9a
Mathieu Beligon <mathieu@feedly.com>
2021-04-23 13:14:51 -0400

    [TripletLoss] Implement post training plugin

--- src/research/mathieu/benchmark_split_mnist.py
+++ src/research/mathieu/benchmark_split_mnist.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from typing import List, Union
 
-import torch
 from avalanche.benchmarks.classic import SplitMNIST
 from avalanche.evaluation.metrics import StreamConfusionMatrix
 from avalanche.training import EvaluationPlugin
@@ -11,15 +10,20 @@ from torch.nn import CrossEntropyLoss
 from torch.optim import SGD
 from tqdm import tqdm
 
-from tricicl.constants import SEEDS, TB_DIR
+from tricicl.cil_memory.memory import CILMemory
+from tricicl.cil_memory.plugin import CILMemoryPlugin
+from tricicl.cil_memory.replay import CILReplayPlugin
+from tricicl.cil_memory.strategy.herding import HerdingMemoryStrategy
+from tricicl.cil_memory.strategy.strategy import CILMemoryStrategyABC
+from tricicl.constants import SEEDS, TB_DIR, device
 from tricicl.loggers.tb import TensorboardLogger
 from tricicl.metrics.confusion_matrix import SortedCMImageCreator
 from tricicl.metrics.normalized_accuracy import NormalizedExperienceAccuracy, NormalizedStreamAccuracy
+from tricicl.models.nme.plugin import NMEPlugin
 from tricicl.models.simple_mlp import SimpleMLP
 from tricicl.strategies.icarl import make_icarl_plugins
+from tricicl.triplet_loss.triplet_loss_post_training_plugin import TripletLossPostTrainingPlugin
 from tricicl.utils.time import create_time_id
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def evaluate_on_seed(
@@ -28,10 +32,11 @@ def evaluate_on_seed(
     seed: int,
     tensorboard_logs_dir: Union[str, Path] = str(TB_DIR),
 ):
-    print(f"Using device {device.type}")
-    perm_mnist = SplitMNIST(n_experiences=5, seed=seed)
 
-    model = SimpleMLP(n_classes=perm_mnist.n_classes, input_size=28 * 28)
+    split_mnist = SplitMNIST(n_experiences=5, seed=seed)
+
+    model = SimpleMLP(n_classes=split_mnist.n_classes, input_size=28 * 28)
+    # model = SimpleCNN(n_channels=1, n_classes=split_mnist.n_classes)
 
     tb_logger = TensorboardLogger(tensorboard_logs_dir + f"/split_mnist/{name}/{seed}_{create_time_id()}")
 
@@ -47,18 +52,56 @@ def evaluate_on_seed(
         evaluator=EvaluationPlugin(
             [NormalizedStreamAccuracy(), NormalizedExperienceAccuracy()],
             StreamConfusionMatrix(
-                num_classes=perm_mnist.n_classes,
-                image_creator=SortedCMImageCreator(perm_mnist.classes_order),
+                num_classes=split_mnist.n_classes,
+                image_creator=SortedCMImageCreator(split_mnist.classes_order),
             ),
-            loggers=[tb_logger],
+            loggers=[
+                tb_logger,
+                # InteractiveLogger(),
+            ],
         ),
     )
 
-    for i, train_task in enumerate(perm_mnist.train_stream, 1):
+    for i, train_task in enumerate(split_mnist.train_stream, 1):
         cl_strategy.train(train_task, num_workers=0)
-        cl_strategy.eval(perm_mnist.test_stream[:i])
+        cl_strategy.eval(split_mnist.test_stream[:i])
 
     tb_logger.writer.flush()
+
+
+def make_tricicl_post_training_plugins(
+    *,
+    memory_size: int,
+    use_replay: bool,
+    use_training_dataloader: bool,
+    classification_loss_coef: float,
+    nme: bool,
+    distillation: bool,
+    memory_strategy: CILMemoryStrategyABC = HerdingMemoryStrategy(),
+    margin: float = 1,
+):
+    memory = CILMemory(memory_size)
+    plugins = [
+        CILMemoryPlugin(memory, memory_strategy=memory_strategy),
+        TripletLossPostTrainingPlugin(
+            memory=memory,
+            margin=margin,
+            use_training_dataloader=use_training_dataloader,
+            classification_loss_coef=classification_loss_coef,
+        ),
+    ]
+    if use_replay:
+        plugins.append(CILReplayPlugin(memory))
+    if nme:
+        plugins.append(NMEPlugin(memory))
+    if distillation:
+        plugins.append(LwFPlugin())
+    return plugins
+
+
+def make_replay_plugins(*, memory_size: int, memory_strategy: CILMemoryStrategyABC = HerdingMemoryStrategy()):
+    memory = CILMemory(memory_size)
+    return [CILMemoryPlugin(memory, memory_strategy=memory_strategy), CILReplayPlugin(memory)]
 
 
 if __name__ == "__main__":
@@ -68,3 +111,95 @@ if __name__ == "__main__":
         evaluate_on_seed("naive", [], seed)
         evaluate_on_seed("replay", [ReplayPlugin()], seed)
         evaluate_on_seed("hybrid1", [ReplayPlugin(), LwFPlugin()], seed)
+        evaluate_on_seed(
+            "tl-post",
+            make_tricicl_post_training_plugins(
+                memory_size=200,
+                use_replay=True,
+                use_training_dataloader=True,
+                classification_loss_coef=0,
+                distillation=False,
+                nme=False,
+            ),
+            seed,
+        )
+        evaluate_on_seed(
+            "tl-post-D",
+            make_tricicl_post_training_plugins(
+                memory_size=200,
+                use_replay=True,
+                use_training_dataloader=True,
+                classification_loss_coef=0,
+                nme=False,
+                distillation=True,
+            ),
+            seed,
+        )
+        evaluate_on_seed(
+            "tl-post-nme",
+            make_tricicl_post_training_plugins(
+                memory_size=200,
+                use_replay=True,
+                use_training_dataloader=True,
+                classification_loss_coef=0,
+                nme=True,
+                distillation=False,
+            ),
+            seed,
+        )
+        evaluate_on_seed(
+            "tl-post-nme-D",
+            make_tricicl_post_training_plugins(
+                memory_size=200,
+                use_replay=True,
+                use_training_dataloader=True,
+                classification_loss_coef=0,
+                nme=True,
+                distillation=True,
+            ),
+            seed,
+        )
+    # evaluate_on_seed("replay", [ReplayPlugin()], SEEDS[0])
+    # evaluate_on_seed("replay-herding", make_replay_plugins(memory_size=200), SEEDS[0])
+    # evaluate_on_seed(
+    #     "tl",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=False, use_training_dataloader=True, classification_loss_coef=0
+    #     ),
+    #     SEEDS[0],
+    # )
+    # evaluate_on_seed(
+    #     "tl-cl.3",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=False, use_training_dataloader=True, classification_loss_coef=0.3
+    #     ),
+    #     SEEDS[0],
+    # )
+    # evaluate_on_seed(
+    #     "tl-replay",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=True, use_training_dataloader=False, classification_loss_coef=0
+    #     ),
+    #     SEEDS[0],
+    # )
+    # evaluate_on_seed(
+    #     "tl-replay-cl.3",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=True, use_training_dataloader=False, classification_loss_coef=0.3
+    #     ),
+    #     SEEDS[0],
+    # )
+    # evaluate_on_seed(
+    #     "tl-replay-dl-cl.3",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=True, use_training_dataloader=True, classification_loss_coef=0.3
+    #     ),
+    #     SEEDS[0],
+    # )
+    # evaluate_on_seed(
+    #     "tl-replay-dl",
+    #     make_tricicl_post_training_plugins(
+    #         memory_size=200, use_replay=True, use_training_dataloader=True, classification_loss_coef=0
+    #     ),
+    #     SEEDS[0],
+    # )
--- src/tricicl/constants.py
+++ src/tricicl/constants.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import torch
+
 from tricicl.utils.path import safe_path
 
 PROJECT_DIR = Path(__file__).parent.parent.parent
@@ -9,3 +11,6 @@ CSV_DIR = safe_path(LOGS_DIR / "csv")
 
 
 SEEDS = [42, 51, 441390, 6584, 601065, 845929, 960932, 478844, 565362, 388222]
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Using device {device.type}")
